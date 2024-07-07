d4feea0092c5cba60c9a3c38f42223d76b7f60cb
ElysiaMae <ElysiaRealme@outlook.com>
2024-07-07 21:09:17 +0800

    🎉 init

---
+++ DevCat/DevCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevCatApp.swift
+//  DevCat
+//
+//  Created by ElysiaMae on 2024/7/7.
+//
+
+import SwiftUI
+
+@main
+struct DevCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
